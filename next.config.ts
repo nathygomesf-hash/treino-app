a85c8804cf433419f6e55cd1cb586870250dbe67
Nathalia Gomes de Freitas <nathalia.freitas@mds.gov.br>
2026-06-01 16:27:09 -0300

    Remove next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
-import withPWAInit from "next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig: NextConfig = {};
 
-export default withPWA(nextConfig);
+export default nextConfig;
